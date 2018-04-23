ebfef0c8c2d99012e1cd4d6c723f2a58ff7acd4e
ira <irene.ungureanu96@gamil.com>
2018-04-24 00:19:32 +0300

    added more functions

--- lab5/src/server.rb
+++ lab5/src/server.rb
@@ -2,11 +2,14 @@ require 'socket'
 require 'pry'
 
 class Server
-  def initialize(socket_address, socket_port)
-    @server_socket = TCPServer.open(socket_port, socket_address)
+  def initialize(socket_port, socket_address)
+    @server_socket = TCPServer.open(socket_address, socket_port)
     @commands = {
-      "/hello" => method(:hello_command),
-      "/help"  => method(:help_command)
+      "/hello"        => method(:hello_command),
+      "/help"         => method(:help_command),
+      "/time_now"     => method(:time_now),
+      "/generate_num" => method(:generate_num),
+      "/coin_flip"    => method(:coin_flip)
     }
 
     puts "Started server.........\n"
@@ -39,18 +42,50 @@ class Server
 
   def help_command
     commands_description = {
-      "/hello <param>" => "Prints 'Hello <param>'",
-      "/help"          => "Prints all the supported commands"
+      "/hello <param>"                  => "Prints 'Hello <param>'",
+      "/time_now"                       => "Prints the current time",
+      "/generate_num <param1> <param2>" => "Prints a randomly generated number between the two <param> values",
+      "/flip_cpin"                      => "Prints the result of flipping a coin",
+      "/help"                           => "Prints all the supported commands"
     }
 
+    result = "\n"
     commands_description.each do |key, value|
-      key + "\t#" + value
+      result << "#{key.ljust(30)}##{value}\n"
     end
+    result
   end
 
   def hello_command(param)
     param = param.join(" ") if param.kind_of?(Array)
     "Hello #{param}"
+  end
+
+  def time_now
+    "#{Time.now}"
+  end
+
+  def generate_num(params)
+    return "Wrong number of arguments" if params.length > 2
+    return "Incorrect parameter type" unless is_number?(params.first) and is_number?(params.last)
+
+    min = params.first.to_i < params.last.to_i ? params.first.to_i : params.last.to_i
+    max = params.first.to_i < params.last.to_i ? params.last.to_i : params.first.to_i
+
+    "\nYour number: #{Random.new.rand(min..max)}\n"
+  end
+
+  def coin_flip
+    result = Random.rand(2)
+    if result == 0
+      "Heads!"
+    else
+      "Tails!"
+    end
+  end
+
+  def is_number?(string)
+    true if Float(string) rescue false
   end
 end
 
